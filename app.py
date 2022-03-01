3ea3e9ad827bb2c06a7d3c448abefee5bd02c27a
shreyas7kha <62547082+shreyas7kha@users.noreply.github.com>
2022-03-01 22:23:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,124 @@
+from opcode import stack_effect
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from plot import plot_pitch
+
+df = pd.read_csv('data/AppData.gz', encoding='utf-8-sig')
+df_fil = df.copy()
+df_fil['isGoal'] = df_fil['isGoal'].apply(abs)
+
+st.title("All About GameStates")
+
+st.text("An app for finding Understat player by player shot data based on their shooting\nand finishing performance in different gamestates")
+st.caption("Made by Shreyas Khatri. Twitter: @khatri_shreyas", unsafe_allow_html=False)
+
+# WIDGETS SIDEBAR
+st.sidebar.header("PLAYER INPUTS")
+player_id = st.sidebar.number_input("Input Understat Player ID:", 
+    format='%d',
+    value = 2097
+)
+start_year, end_year = st.sidebar.select_slider(
+     "Select Range of Seasons:",
+     options=[x for x in range(2014, 2021)],
+     value=(2014,2020)
+)
+filter_gs = st.sidebar.multiselect(
+    "Filter for Game States:",
+    options = df['Filter Game_State'].unique().tolist(),
+    default = df['Filter Game_State'].unique().tolist()
+)
+player_col = st.sidebar.color_picker('Pick A Color', '#EA2304')
+theme = st.sidebar.radio(
+     "Visualisation Theme",
+     ('dark', 'light')
+)
+only_goals = st.sidebar.checkbox('Show Only Goals')
+add_pens = st.sidebar.checkbox('Keep Penalties')
+only_changed_gs = st.sidebar.checkbox('Show Only Game State Altering Moments')
+
+# FILTER DF
+df_fil = df_fil[df_fil['Player ID']==player_id]
+df_fil = df_fil[df_fil['Season'].between(start_year, end_year)]
+df_fil = df_fil[df_fil['Filter Game_State'].isin(filter_gs)]
+if only_goals:
+    df_fil = df_fil[df_fil['Shot Result']=='Goal']
+if not add_pens:
+    df_fil = df_fil[df_fil['Situation']!='Penalty']
+if only_changed_gs:
+    df_fil = df_fil[df_fil['Changed Game State']==True]
+
+def find_distance(x, y):
+    return np.sqrt(np.sum(np.square(np.array([100, 50])-np.array([x*100, y*100]))))
+# GROUPBY DF
+df_gb = df_fil[['Player ID','x','y', 'Shot xG','isGoal','Filter Game_State']].groupby("Filter Game_State").agg(['sum', 'count'])
+df_gb['Distance'] = df_gb.apply(lambda x: find_distance(x['x']['sum']/x['x']['count'], x['y']['sum']/x['y']['count']), axis=1)
+df_gb = df_gb.drop([('Player ID',   'sum'), (  'Shot xG', 'count'), (   'isGoal', 'count'),
+                    (   'x', 'count'), (   'x', 'sum'), (   'y', 'count'), (   'y', 'sum')], axis=1)
+df_gb.columns = ['Total Shots', 'Total xG', 'Total Goals', 'Distance From Goal']
+df_gb['xG Overperformance'] = df_gb['Total Goals'] - df_gb['Total xG']
+df_gb['xG O/P per Shot'] = df_gb['xG Overperformance']/df_gb['Total Shots']
+
+
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8-sig')
+
+# INTERFACE
+if len(df_fil) > 0:
+    st.header("\n\nPlayer: {}".format(df_fil['Player'].unique()[0]))
+    st.subheader("\nPLAYER SHOT BY GAMESTATE COMPARISON")
+    st.table(df_gb)
+else:
+    st.error("Sorry no entries with these inputs, please change your inputs")
+    
+if st.button('Click For Visualisation Generation'):
+    st.header("PLAYER SHOT DATAFRAME")
+    st.dataframe(df_fil.drop(['x','y', 'h_a', 'isGoal', 'Filter Game_State'], axis=1).reset_index(drop=True), width = 2000, height=250)
+    df_fil_save = convert_df(df_fil)
+    
+    st.download_button(
+        label="Download Shot DataFrame as CSV",
+        data=df_fil_save,
+        file_name='PlayerShots.csv',
+        mime='text/csv',
+    )
+
+    theme_dict = {'light':'white', 'dark':'black'}
+    st.header("\nPLAYER SHOT MAP")
+    fig, ax = plot_pitch(df_fil, theme=theme, player_col=player_col)
+    plt.savefig('static/PlayerShots.png', dpi=300, facecolor=theme_dict[theme])
+    st.pyplot(fig)
+    with open("static/PlayerShots.png", "rb") as file:
+        btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="PlayerShots.png",
+            mime="image/png"
+           )
+
+
+
+# GUIDE AND CREDITS
+st.markdown("""
+    ### GUIDE TO THE WIDGET ELEMENTS
+
+    1. Player ID - Player ID for player on Understat.com(https://www.understat.com)
+    2. Range Of Season - Filter for seasons of relevance
+    3. Filter Game State - Shots during a particular type (or types) of game state
+    4. Show Only Goals - Filter for only goals
+    5. Keep Penalties - Do you want to keep penalties or not?
+    6. Game State Altering Moments - Show only those goals which altered the game state
+    7. Pick A Colour - Choose colour for goal scatter points
+    8. Visualisation Theme - Choose between light and dark themed templates
+""")
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
---
+++ plot.py
@@ -0,0 +1,89 @@
+import mplsoccer
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.patches as patches
+
+import numpy as np
+
+mpl.rcParams['font.family']= 'Century Gothic'
+mpl.rcParams['font.size'] = 12
+mpl.rcParams['font.weight'] = 'bold'
+mpl.rcParams['legend.title_fontsize'] = 20
+mpl.rcParams['legend.fontsize'] = 17
+
+def dark_theme():
+    mpl.rcParams['text.color'] = 'white'
+    return 'white', 'black'
+
+def light_theme():
+    mpl.rcParams['text.color'] = 'black'
+    return 'black', 'white'
+
+
+def plot_pitch(df, theme = 'light', player_col='red'):
+    if theme == 'light':
+        point_color, bg_color = light_theme()
+    elif theme == 'dark':
+        point_color, bg_color = dark_theme()
+    gold_col = '#ad993c'
+    in_col = '#868686'
+
+
+    fig, ax = plt.subplots()
+    fig.set_facecolor(bg_color)
+    pitch = mplsoccer.VerticalPitch(pitch_type='opta', pitch_color=bg_color,
+                        linewidth=1.5, line_alpha=0.5, goal_alpha=0.5, half=True)
+    pitch.draw(ax=ax)
+
+    if len(df) == 0:
+        return fig, ax
+
+    # SCATTER POINTS
+    goal_cond = df['Shot Result']=='Goal'
+    pitch.scatter(df[~goal_cond]['x']*100, df[~goal_cond]['y']*100, c='none', ec=point_color, alpha=0.15, ax=ax)
+    pitch.scatter(df[goal_cond]['x']*100, df[goal_cond]['y']*100, c='none', ec=player_col, hatch='////////', alpha=0.5, ax=ax)
+
+    # INTERNAL DATA
+    player = df['Player'].unique()[0]
+    start_year = df['Season'].min()
+    end_year = df['Season'].max()
+    avg_x, avg_y = df['x'].mean()*100, df['y'].mean()*100
+    total_goals = sum(goal_cond)
+    total_shots = len(df)
+    total_xg = df['Shot xG'].sum()
+    avg_distance_to_goal = np.sqrt(np.sum(np.square(np.array([100, 50])-
+                                                    np.array([avg_x, avg_y]))))
+    
+    # DISTANCE
+    circle = patches.Circle((50, 100), avg_distance_to_goal, ec=point_color, fc='None', alpha=0.5, ls=':', lw=1.5)
+    ax.add_patch(circle)
+    ax.annotate("", xy=(50-avg_distance_to_goal, 102.5), xycoords='data', 
+                xytext=(50, 102.5), textcoords='data',
+                size=10, arrowprops=dict(arrowstyle="-|>",fc='none', ls='--', ec=player_col))
+    ax.text(49-avg_distance_to_goal, 102.5, "{:.2f}m".format(avg_distance_to_goal), size=11.5, 
+            ha='left', va='center',c=in_col)
+    ax.text(51, 102.5, "Avg distance", size=8, ha='right', va='center')
+
+    # TEXT
+    ax.scatter(np.linspace(90,10,5), [56]*5, c=bg_color, ec=point_color, marker='h', s=1500, ls='--', alpha=0.5)
+    
+    ax.text(90, 56,'{}'.format(total_shots), size=13, color=player_col, ha='center', va='center')
+    ax.text(70, 56,'{}'.format(total_goals), size=13, color=player_col, ha='center', va='center')
+    ax.text(50, 56,'{:.2f}'.format((total_goals-total_xg)/total_shots), size=13, color=player_col, ha='center', va='center')
+    ax.text(30, 56,'{:.2f}'.format(total_xg/total_shots), size=13, color=player_col, ha='center', va='center')
+    ax.text(10, 56,'{}'.format(sum(df['Changed Game State']==True)), size=13, color=player_col, ha='center', va='center')
+    
+    
+    ax.text(90, 51.5,'Total\nShots', size=8, color=in_col, ha='center', va='top')
+    ax.text(70, 51.5,'Total\nGoals', size=8, color=in_col, ha='center', va='top')
+    ax.text(50, 51.5,'Finishing\nOverperformance\nper Shot', size=8, color=in_col, ha='center', va='top')
+    ax.text(30, 51.5,'xG\nper Shot', size=8, color=in_col, ha='center', va='top')
+    ax.text(10, 51.5,'Number of\nGame-State\nAltering Goals', size=8, color=in_col, ha='center', va='top')
+
+    # ADD TITLES
+    ax.set_title(f'{player.upper()}', color=gold_col, loc='left', 
+        pad=25, weight='heavy', size=22)
+    ax.text(103, 104.5,  f'{start_year}-{end_year+1} | League games only', size=13)
+    fig.text(0.15, 0.05, 'Made using All About GameStates App. By @khatri_shreyas.', size=7)
+
+    return fig, ax
